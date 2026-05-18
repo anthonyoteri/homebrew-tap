f37252484fd4c668556a7c9b28eecdeef61c6764
Anthony Oteri <4360016+anthonyoteri@users.noreply.github.com>
2026-05-18 07:43:51 -0400

    structprop-validator 0.4.0

--- Formula/structprop-validator.rb
+++ Formula/structprop-validator.rb
@@ -1,24 +1,24 @@
 class StructpropValidator < Formula
   desc "Validator for structprop configuration files"
   homepage "https://github.com/anthonyoteri/serde-structprop"
-  version "0.3.0"
+  version "0.4.0"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
       url "https://github.com/anthonyoteri/serde-structprop/releases/download/v#{version}/structprop-validator-macos-aarch64.tar.gz"
-      sha256 "01e111812a80113d8dd144f5b9f5537cb3c4c278b9bf7f46d6dccd9af2b276a7"
+      sha256 "8bdc46295faa3329d2030182cceee96b6bd8eea12b9d85be3a2b329e0d41c4a5"
     end
     on_intel do
       url "https://github.com/anthonyoteri/serde-structprop/releases/download/v#{version}/structprop-validator-macos-x86_64.tar.gz"
-      sha256 "ea318f005e612c7f34015f850aa10f374dd9d767abdd5ac254999a98199e7fab"
+      sha256 "f519fe9c7461540a5066a7b57882ff35603b6ffc44da6d59e312c8a8e1bb749d"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/anthonyoteri/serde-structprop/releases/download/v#{version}/structprop-validator-linux-x86_64.tar.gz"
-      sha256 "1e0cbc4e1c198fe149f11e83faae05441fa64e9c4ecb3f3792a1c94aa64aa1ef"
+      sha256 "450d36b98357dc57dc678836c2dd31bfe67bde934d55351e58c3c3314c046f14"
     end
   end
 

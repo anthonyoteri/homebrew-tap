a416f91a933b8bacad516835eba42709bf7281ba
Anthony Oteri <4360016+anthonyoteri@users.noreply.github.com>
2026-06-01 13:13:24 -0400

    chore: bump cargo-rigtest to v0.2.1

--- Formula/cargo-rigtest.rb
+++ Formula/cargo-rigtest.rb
@@ -1,28 +1,28 @@
 class CargoRigtest < Formula
   desc "Cargo subcommand for acceptance/integration testing with rigtest"
   homepage "https://github.com/anthonyoteri/cargo-rigtest"
-  version "0.2.0"
+  version "0.2.1"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
       url "https://github.com/anthonyoteri/cargo-rigtest/releases/download/v#{version}/cargo-rigtest-macos-aarch64.tar.gz"
-      sha256 "c1d5872fc7ea904e4e553d2700596775b02340c40b4591f0e13bd751b60637ea"
+      sha256 "f10f4f38809c2ac4540bb7aaec44783862126b2e267591a9847b785434c6a127"
     end
     on_intel do
       url "https://github.com/anthonyoteri/cargo-rigtest/releases/download/v#{version}/cargo-rigtest-macos-x86_64.tar.gz"
-      sha256 "b5cb79e39649517f2577861330f189ae540de4d138a839c8421d2075af81463b"
+      sha256 "8018836c90582c9f6b571d0298c99dda391ad3acebeeb3a0b73992a056305ade"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/anthonyoteri/cargo-rigtest/releases/download/v#{version}/cargo-rigtest-linux-x86_64.tar.gz"
-      sha256 "515d5d834cf26a4a9c61730d6185b18d4e3423891f8b8401d829fe6b82467778"
+      sha256 "83c7751eed70420cff17b56aa8ddb503463353e98febcd976ea0f6b2cc3e7553"
     end
     on_arm do
       url "https://github.com/anthonyoteri/cargo-rigtest/releases/download/v#{version}/cargo-rigtest-linux-aarch64.tar.gz"
-      sha256 "9fb26923229b6f3d9fb1c4a697ff1419fa2e670978064a7f5849c5fafe3d693d"
+      sha256 "3340323966006cc08414e3d00d50deeeaa47a5be3d48bc54a57a32a98ba3f3c6"
     end
   end
 

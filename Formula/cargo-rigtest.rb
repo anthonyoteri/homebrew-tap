63f4d4fbfd2987540badb7bb24b88c8305945413
Anthony Oteri <4360016+anthonyoteri@users.noreply.github.com>
2026-06-10 08:03:00 -0400

    cargo-rigtest 0.4.0
    Adds --reporter junit for JUnit XML output.

--- Formula/cargo-rigtest.rb
+++ Formula/cargo-rigtest.rb
@@ -1,28 +1,28 @@
 class CargoRigtest < Formula
   desc "Cargo subcommand for acceptance/integration testing with rigtest"
   homepage "https://github.com/anthonyoteri/cargo-rigtest"
-  version "0.3.1"
+  version "0.4.0"
   license any_of: ["MIT", "Apache-2.0"]
 
   on_macos do
     on_arm do
       url "https://github.com/anthonyoteri/cargo-rigtest/releases/download/v#{version}/cargo-rigtest-macos-aarch64.tar.gz"
-      sha256 "67ffc13821cdadc6cc16099fc76ade7fe36567c71fc5f5a7d59dd5131eb80e62"
+      sha256 "4a8b838c64c78c965272eebfa6d31d4eb1e30c6b07ab43d179f8671a4c61e55c"
     end
     on_intel do
       url "https://github.com/anthonyoteri/cargo-rigtest/releases/download/v#{version}/cargo-rigtest-macos-x86_64.tar.gz"
-      sha256 "f80024b6bea25c50a1b0a9fe3b400c620faccd22c86693ce62dfc4a69ee61568"
+      sha256 "7ad119cbf0d6822877314d3ebe8eb01f3e0c2b384260395964c996a96b1eb383"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/anthonyoteri/cargo-rigtest/releases/download/v#{version}/cargo-rigtest-linux-x86_64.tar.gz"
-      sha256 "ede258c75920e00475f213868f2c54585676a3fe8f34d8a93986f62894b653d3"
+      sha256 "c643bbc84dd5d1e8041de8296a2e4d39f2d3ea53fb14b71d94b09f747718b4ce"
     end
     on_arm do
       url "https://github.com/anthonyoteri/cargo-rigtest/releases/download/v#{version}/cargo-rigtest-linux-aarch64.tar.gz"
-      sha256 "e01fdc043ef151c4c58c0ea40cca1d0ea332b48b7bc46436ac2948ec43d60e8d"
+      sha256 "81aa8463e203233846506903ac74410198652e9c114723e2c11ac1ce22ae8ea4"
     end
   end
 
